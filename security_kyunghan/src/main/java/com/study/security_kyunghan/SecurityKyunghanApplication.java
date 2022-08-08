0b1e9c05681d8d3b08ee95a57064b84ba8587fa5
asdf4026 <i1427@naver.com>
2022-08-08 19:13:09 +0900

    security

---
+++ security_kyunghan/src/main/java/com/study/security_kyunghan/SecurityKyunghanApplication.java
@@ -0,0 +1,13 @@
+package com.study.security_kyunghan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityKyunghanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityKyunghanApplication.class, args);
+	}
+
+}
